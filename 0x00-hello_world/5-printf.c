dd17e70923d0590581deea2ea4897915414c5917
walter-cumbucane <cumbucane.walter@gmail.com>
2023-06-15 15:52:45 -0700

    Task 5 done

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main(void)
+{
+	/*This function prints out a text on the standard output */
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
