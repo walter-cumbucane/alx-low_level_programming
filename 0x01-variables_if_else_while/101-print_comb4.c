1e0ae51f70a0507dad2393246379fdc768eaf7ab
walter-cumbucane <cumbucane.walter@gmail.com>
2023-06-16 16:43:14 -0700

    Task 11 done

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: 'the program's description'
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int i = 48;
+	int j;
+	int z;
+
+	while (i <= 55)
+	{	j = i + 1;
+		while (j <= 56)
+		{
+			z = j + 1;
+			while (z <= 57)
+			{
+				putchar(i);
+				putchar(j);
+				putchar(z);
+				if (i == 55 && j == 56 && z ==57)
+				{
+					putchar('\n');
+				}
+				else
+				{
+					putchar(',');
+					putchar(' ');
+				}
+				z++;
+			}
+			j++;
+		}
+		i++;
+	}
+	return (0);
+}
