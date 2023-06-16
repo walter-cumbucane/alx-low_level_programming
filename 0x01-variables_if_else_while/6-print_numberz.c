acb6fb7c081aeb55275adde86a4a0e770f370a1a
walter-cumbucane <cumbucane.walter@gmail.com>
2023-06-16 16:06:33 -0700

    Task 6 done

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: 'the program's description'
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int i = 48;
+
+	while (i <= 57)
+	{
+		putchar(i);
+		if (i == 57)
+		{
+			putchar('\n');
+		}
+		i++;
+	}
+	return (0);
+}
