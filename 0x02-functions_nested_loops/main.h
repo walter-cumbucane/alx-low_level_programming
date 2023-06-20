55201e9c9f98c8282ec48873cc6663691690d4ac
walter-cumbucane <cumbucane.walter@gmail.com>
2023-06-20 06:54:55 -0700

    Challenging

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,5 +1,5 @@
-// It is not recommended to put function definitions 
-// in a header file. Ideally there should be only
-// function declarations.
+/* * It is not recommended to put function definitions 
+* in a header file. Ideally there should be only
+* function declarations.*/
 
 int _putchar(char c);
