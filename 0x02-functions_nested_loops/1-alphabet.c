8c03a1997dd1d783a9c422046f43b7daeb6bc36b
walter-cumbucane <cumbucane.walter@gmail.com>
2023-06-20 07:30:25 -0700

    I'm starting to understand it

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * main - Entry point
+ *
+ * Description: 'the program's description'
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	print_alphabet();
+	return (0);
+}
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,5 +1,29 @@
 /* * It is not recommended to put function definitions 
 * in a header file. Ideally there should be only
-* function declarations.*/
+* function declarations. Purpose of this code is
+* to only demonstrate working of header files.
+*/
 
-int _putchar(char c);
+
+/* *_putchar function declaration */
+#include <unistd.h>
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
+}
+
+
+/* * Task 1 function declaration */
+void print_alphabet(void)
+{
+	int i = 97;
+	while (i <= 122)
+	{
+		_putchar(i);
+		if (i == 122)
+		{
+			_putchar('\n');
+		}
+		i++;
+	}
+}
