21a25c12df99d0d670a638c7c6e3c848ed4b2f8c
walter-cumbucane <cumbucane.walter@gmail.com>
2023-06-21 07:25:34 -0700

    Task 0 redone

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,18 +1,16 @@
 #include "main.h"
 
 /**
- * main - Entry point
- *
- * Description: 'the program's description'
- *
- * Return: Always 0 (Success)
- */
+* main - tests function that prints if integer is positive or negative
+* Return: 0
+*/
 
 int main(void)
 {
-	int i;
+        int i;
 
-	i = 0;
-	positive_or_negative(i);
-	return (0);
+        i = 0;
+        positive_or_negative(i);
+
+        return (0);
 }
