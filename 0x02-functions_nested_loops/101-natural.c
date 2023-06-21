a3a159f6d7ad21089fd5f58d08430fb3738b346e
walter-cumbucane <cumbucane.walter@gmail.com>
2023-06-20 18:45:31 -0700

    Task 13 done

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: 'the program's description'
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int sum;
+	int i;
+	
+	sum = 0;
+	while (i <1024)
+	{
+		if ((i % 3 == 0) && (i % 5 == 0))
+		{
+			sum += i;
+		}
+	}
+}
