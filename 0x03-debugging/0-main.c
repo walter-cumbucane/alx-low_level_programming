e29501967dd6f5c726bc16b99a41ba21d03af680
walter-cumbucane <cumbucane.walter@gmail.com>
2023-06-21 07:21:33 -0700

    Task 0 done

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * main - Entry point
+ *
+ * Description: 'the program's description'
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int i;
+
+	i = 0;
+	positive_or_negative(i);
+	return (0);
+}
