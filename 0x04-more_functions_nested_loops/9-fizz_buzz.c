28c1fac26497fde24f51ef4921182965de5806ee
walter-cumbucane <cumbucane.walter@gmail.com>
2023-06-22 17:29:39 -0700

    Yeah, programming is very fun!

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: 'the program's description'
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int i;
+
+	i = 1;
+	while (i <= 100)
+	{
+		if ((i % 3 == 0) && (i % 5 == 0))
+		{
+			printf("FizzBuzz ");
+		}
+		else if (i % 5 == 0)
+		{
+			printf("Buzz ");
+		}
+		else if (i % 3 == 0)
+		{
+			printf("Fizz ");
+		}
+		else
+		{
+			printf("%i ", i);
+		}
+		if (i == 100)
+		{
+			putchar('\n');
+		}
+		i++;
+	}
+	return (0);
+}
+
