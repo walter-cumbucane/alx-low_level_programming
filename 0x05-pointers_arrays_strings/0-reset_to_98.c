26d40ced2d2af320ad36ee12865cd79c2447d9ee
walter-cumbucane <cumbucane.walter@gmail.com>
2023-06-26 12:04:23 -0700

    Task 0 done

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,11 @@
+/**
+ * reset_to_98 - set the integer to 402
+ * @n: a pointer the integer we want to set to 98
+ *
+ * Return: nothing
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/main.h
@@ -0,0 +1,12 @@
+/* * It is not recommended to put function definitions 
+* in a header file. Ideally there should be only
+* function declarations. Purpose of this code is
+* to only demonstrate working of header files.
+*/
+
+
+/* * _putchar function declaration */
+int _putchar(char c);
+
+/* * task 0 function declaration */
+void reset_to_98(int *n);
