79366313ef812cf9b7c217d17169ab2f4bf39f9c
walter-cumbucane <cumbucane.walter@gmail.com>
2023-07-06 12:34:38 -0700

    Task 8 done

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -7,25 +7,22 @@
 
 int wildcmp(char *s1, char *s2)
 {
-	int result;
+	if (*s1 == 0)
+	{
+		if (*s2 != 0 && *s2 == '*')
+		{
+			return (wildcmp(s1, s2 + 1));
+		}
+		return (*s2 == 0);
+	}
 
 	if (*s2 == '*')
 	{
-		return (wildcmp(s1, s2 + 1) || wildcmp(s1 + 1, s2));
+		return (wildcmp(s1 + 1, s2) || wildcmp(s1, s2 + 1));
 	}
-
-	if (*s1 == 0 && *s2 == 0)
+	else if (*s1 == *s2)
 	{
-		return (1);
+		return (wildcmp(s1 + 1, s2 + 1));
 	}
-	if (*s1 == *s2)
-	{
-		result = 1;
-		result *= wildcmp(s1 + 1, s2 + 1);
-		return (result);
-	}
-	else
-	{
-		return (0);
-	}
+	return (0);
 }
