c6c7b185cdb5e5a6bb26db7e25b75a4b52cfc8d6
walter-cumbucane <cumbucane.walter@gmail.com>
2023-07-07 20:47:07 +0200

    Task 0 done

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *@argc : number of arguments inserted
+ @argv : array of strings, where each string is an argument that was inserted
+ *
+ * Description: 'the program's description'
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(int argc, char **argv)
+{
+	printf("%s\n", *argv);
+	return (0);
+}
