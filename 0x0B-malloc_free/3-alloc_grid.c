85a980e340e877574f2cf64456d24b2116419867
walter-cumbucane <cumbucane.walter@gmail.com>
2023-07-11 00:50:45 +0200

    Task 3 done

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -30,7 +30,7 @@ int **alloc_grid(int width, int height)
 		if (*(arr + i) == NULL)
 		{
 			for (i--; i <= 0; i--)
-				free(*(arr + i);
+				free(*(arr + i));
 			free(arr);
 			return (NULL);
 		}
