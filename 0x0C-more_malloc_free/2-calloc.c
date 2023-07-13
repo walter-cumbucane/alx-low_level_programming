33abcedcb4b7521977894cda4c956fe106d54fe5
walter-cumbucane <cumbucane.walter@gmail.com>
2023-07-14 00:53:46 +0200

    Task 2 done

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -21,7 +21,7 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 	{
 		return (NULL);
 	}
-	for (i = 0; i < nmemb; i++)
-		*(s + i) = 0;
+	for (i = 0; i < nmemb * size; i++)
+		*((char *)s + i) = 0;
 	return (s);
 }
