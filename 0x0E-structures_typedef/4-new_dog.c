a32d26f43e030a040caba3e81e7b3f1167788bdc
walter-cumbucane <cumbucane.walter@gmail.com>
2023-07-18 04:43:16 +0200

    Task 4 done

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -1,6 +1,58 @@
 #include "dog.h"
 #include <stdlib.h>
 
+
+/**
+ * _strcpy - set the integer to 402
+ * @dest: a pointer the integer we want to set to 98
+ * @src : source message
+ * Return: nothing
+ */
+
+char *_strcpy(char *dest, char *src)
+{
+	int i;
+
+	i = 0;
+	while (7 > 5)
+	{
+		if (*(src + i) == 0)
+		{
+			*(dest + i) = 0;
+			break;
+		}
+		else
+		{
+			*(dest + i) = *(src + i);
+			i++;
+		}
+	}
+	return (dest);
+}
+
+
+/**
+ * _strlen - set the integer to 402
+ * @s: a pointer the integer we want to set to 98
+ *
+ * Return: nothing
+ */
+
+int _strlen(char *s)
+{
+	int len;
+	int i;
+
+	len = 0;
+	i = 0;
+	while (*(s + i) != '\0')
+	{
+		len++;
+		i++;
+	}
+	return (len);
+}
+
 /**
  * new_dog - set the integer to 402
  * @name : a letter being tested
@@ -11,16 +63,34 @@
 
 dog_t *new_dog(char *name, float age, char *owner)
 {
-	dog_t d;
 	dog_t *ptr;
+	int len1, len2;
 
-	ptr = &d;
 	if (name == NULL || owner == NULL)
+		return (NULL);
+	ptr = malloc(sizeof(dog_t));
+	if (ptr == NULL)
 	{
+		free(ptr);
 		return (NULL);
 	}
-	(*ptr).name = name;
+	len1 = _strlen(name);
+	len2 = _strlen(owner);
+	(*ptr).name = malloc(len1 + 1);
+	if ((*ptr).name == NULL)
+	{
+		free(ptr);
+		return (NULL);
+	}
+	(*ptr).owner = malloc(len2 + 1);
+	if ((*ptr).owner == NULL)
+	{
+		free((*ptr).name);
+		free(ptr);
+		return (NULL);
+	}
+	(*ptr).name = _strcpy((*ptr).name, name);
+	(*ptr).owner = _strcpy((*ptr).owner, owner);
 	(*ptr).age = age;
-	(*ptr).owner = owner;
 	return (ptr);
 }
--- 0x0E-structures_typedef/main.c
+++ 0x0E-structures_typedef/main.c
@@ -11,11 +11,6 @@ int main(void)
     dog_t *my_dog;
 
     my_dog = new_dog(NULL, 3.5, "Bob");
-    if (my_dog == NULL)
-	{
-		printf("DONE");
-		return (0);
-	}
     printf("My name is %s, and I am %.1f :) - Woof!\n", my_dog->name, my_dog->age);
     return (0);
 }
