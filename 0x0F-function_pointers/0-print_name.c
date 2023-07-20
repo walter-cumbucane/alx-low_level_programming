7f3e662014aec0262047b1f8371c0a2c90389fda
walter-cumbucane <cumbucane.walter@gmail.com>
2023-07-20 03:31:36 +0200

    Task 0 done

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,4 +1,5 @@
 #include "function_pointers.h"
+#include <stdlib.h>
 
 /**
  * print_name - checks for a lowercase character
@@ -8,6 +9,10 @@
  */
 
 void print_name(char *name, void (*f)(char *))
-{
+{	
+	if (f == NULL)
+	{
+		return;
+	}
 	(*f)(name);
 }
