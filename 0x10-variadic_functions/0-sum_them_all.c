ab0402253bdf006903e8e790859b44f641473528
walter-cumbucane <cumbucane.walter@gmail.com>
2023-07-22 14:35:00 +0200

    Task 0 done

---
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -0,0 +1,27 @@
+#include "varadic_functions.h"
+#include <stdarg.h>
+
+/**
+ * sum_them_all - checks for a lowercase character
+ *@n: letter being tested
+ *@...: unkown arguments
+ * Return: Always 0
+ *
+ */
+
+int sum_them_all(const unsigned int n, ...)
+{
+	unsigned int i;
+	int sum;
+	va_list ptr;
+
+	va_start(ptr, n);
+	sum = 0;
+	if (n == 0)
+	{
+		return (0);
+	}
+	for (i = 0; i < count; i++)
+		sum += va_arg(ptr, int);
+	return (sum);
+}
---
+++ 0x10-variadic_functions/varadic_functions.h
@@ -0,0 +1,11 @@
+#ifndef VARADIC_FUNCTIONS_H
+#define VARADIC_FUNCTIONS_H
+
+/* *_putchar function declaration */
+int _putchar(char c);
+
+/* * task 0 function declaration */
+int sum_them_all(const unsigned int n, ...);
+
+
+#endif
