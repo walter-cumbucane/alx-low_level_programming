c451bb499b30bae28d8b3d9500d3158be58d73c4
walter-cumbucane <cumbucane.walter@gmail.com>
2023-07-28 03:11:23 +0200

    Task 2 done

---
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -0,0 +1,47 @@
+#include "lists.h"
+#include <string.h>
+
+/**
+ * _strlen - set the integer to 402
+ * @s: a pointer the integer we want to set to 98
+ *
+ * Return: nothing
+ */
+
+int _strlen(char *s)
+{
+	int len;
+	int i;
+
+	len = 0;
+	i = 0;
+	while (*(s + i) != '\0')
+	{
+		len++;
+		i++;
+	}
+	return (len);
+}
+
+/**
+ * add_node - adds a node to the beginning of a singly linked list
+ * @head: a pointer to the beginning of the first node of a SLL
+ * @str : a string to be stored in the new node of the SLL
+ *
+ * Return: a pointer to the newly created node
+ */
+
+
+list_t *add_node(list_t **head, const char *str)
+{
+	list_t *current;
+
+	current = *head;
+	*head = malloc(sizeof(list_t));
+	if (*head == NULL)
+		return (NULL);
+	(*head)->str = strdup(str);
+	(*head)->len = _strlen((*head)->str);
+	(*head)->next = current;
+	return (*head);
+}
