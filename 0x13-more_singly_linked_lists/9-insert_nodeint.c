884b5e0e5aaad660503a1c2d05117241ae13ccea
walter-cumbucane <cumbucane.walter@gmail.com>
2023-08-01 21:06:31 +0200

    Task 9 done

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -15,7 +15,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	unsigned int i = 0;
 	listint_t *current, *previous, *new;
 
-	if (head == NULL || *head == NULL)
+	if (*head == NULL && idx != 0)
 		return (NULL);
 	current = *head;
 	while (current != NULL)
@@ -44,7 +44,10 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 			return (NULL);
 		new->next = NULL;
 		new->n = n;
-		previous->next = new;
+		if (*head == NULL)
+			*head = new;
+		else
+			previous->next = new;
 		return (new);
 	}
 	return (NULL);
--- 0x13-more_singly_linked_lists/main.c
+++ 0x13-more_singly_linked_lists/main.c
@@ -11,7 +11,7 @@
 int main(void)
 {
     listint_t *head;
-
+/*
     head = NULL;
     add_nodeint_end(&head, 0);
     add_nodeint_end(&head, 1);
@@ -22,8 +22,10 @@ int main(void)
     add_nodeint_end(&head, 402);
     add_nodeint_end(&head, 1024);
     print_listint(head);
+    */
     printf("-----------------\n");
-    insert_nodeint_at_index(&head, 9, 4096);
+    head = NULL;
+    insert_nodeint_at_index(&head, 0, 4096);
     print_listint(head);
     free_listint2(&head);
     return (0);
