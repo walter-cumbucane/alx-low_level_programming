198572502c85fd1e84d16a22653c3db76b00caa2
walter-cumbucane <cumbucane.walter@gmail.com>
2023-08-03 21:12:53 +0200

    Let's get started

---
+++ 0x14-bit_manipulation/main.h
@@ -0,0 +1,10 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/* _putchar function declaration */
+int _putchar(char c);
+
+/* task 0 function declaration */
+unsigned int binary_to_unit(const char *b);
+
+#endif
