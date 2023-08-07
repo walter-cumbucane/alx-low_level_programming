a3d6b63ade6d8df46336f1860ee448af0fcb750c
walter-cumbucane <cumbucane.walter@gmail.com>
2023-08-07 22:57:11 +0200

    Task 0 done

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -4,6 +4,28 @@
 
 
 /**
+ * _strlen - set the integer to 402
+ * @s: a pointer the integer we want to set to 98
+ *
+ * Return: nothing
+ */
+size_t _strlen(char *s)
+{
+	int len;
+	int i;
+
+	len = 0;
+	i = 0;
+	while (*(s + i) != '\0')
+	{
+		len++;
+		i++;
+	}
+	return (len);
+}
+
+
+/**
  * read_textfile - set the integer to 402
  * @filename: a pointer the integer we want to set to 98
  * @letters : number of letters to be printed and read
@@ -27,7 +49,10 @@ size_t read_textfile(const char *filename, size_t letters)
 	if (check == -1)
 		return (0);
 	*(file_content + letters) = '\0';
-	check = write(STDOUT_FILENO, file_content, letters);
+	if (letters > _strlen(file_content))
+			check = write(STDOUT_FILENO, file_content, _strlen(file_content));
+	else
+		check = write(STDOUT_FILENO, file_content, letters);
 	if (check == -1)
 		return (0);
 	return (check);
