2045de9e3fbc9c60eff0fee3424b240f75ef3e38
walter-cumbucane <cumbucane.walter@gmail.com>
2023-08-08 00:29:37 +0200

    Task 1 done

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,51 @@
+#include "main.h"
+#include <fcntl.h>
+#include <unistd.h>
+
+/**
+ * _strlen - set the integer to 402
+ * @s: a pointer the integer we want to set to 98
+ *
+ * Return: nothing
+ */
+size_t _strlen(char *s)
+{
+	int len;
+	int i;
+
+	len = 0;
+	i = 0;
+	while (*(s + i) != '\0')
+	{
+		len++;
+		i++;
+	}
+	return (len);
+}
+
+
+/**
+ * create_file - set the integer to 402
+ * @filename: a pointer the integer we want to set to 98
+ * @text_content : number of letters to be printed and read
+ * Return: the number of letters it could read and print
+ */
+
+int create_file(const char *filename, char *text_content)
+{
+	int fd;
+	int check;
+
+	if (filename == NULL)
+		return (-1);
+
+	fd = open(filename, O_CREAT | O_RDWR, 400);
+	if (fd == -1)
+		return (-1);
+	if (text_content == NULL)
+		return (1);
+	check = write(fd, text_content, _strlen(text_content));
+	if (check == -1)
+		return (-1);
+	return (1);
+}
--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -8,4 +8,7 @@ int _putchar(char c);
 /* task 0 function declaration */
 size_t read_textfile(const char *filename, size_t letters);
 
+/* task 1 function declaration */
+int create_file(const char *filename, char *text_content);
+
 #endif
