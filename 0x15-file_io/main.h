8787f3bf64f5a63a1bce99d68a53d46818db8cd3
walter-cumbucane <cumbucane.walter@gmail.com>
2023-08-07 21:40:43 +0200

    Let's get started

---
+++ 0x15-file_io/main.h
@@ -0,0 +1,11 @@
+#ifndef MAIN_H
+#define MAIN_H
+#include <stdlib.h>
+
+/* _putchar function declaration */
+int _putchar(char c);
+
+/* task 0 function declaration */
+size_t read_textfile(const char *filename, size_t letters);
+
+#endif
