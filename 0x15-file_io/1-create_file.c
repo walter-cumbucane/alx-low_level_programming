d8d411b9bbbeeff796d7413e9f835aa7bfdcf90e
walter-cumbucane <cumbucane.walter@gmail.com>
2023-08-08 00:32:57 +0200

    Task 1 done

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -43,9 +43,16 @@ int create_file(const char *filename, char *text_content)
 	if (fd == -1)
 		return (-1);
 	if (text_content == NULL)
+	{
+		close(fd);
 		return (1);
+	}
 	check = write(fd, text_content, _strlen(text_content));
 	if (check == -1)
+	{
+		close(fd);
 		return (-1);
+	}
+	close(fd);
 	return (1);
 }
