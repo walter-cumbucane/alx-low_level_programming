ab256a36506baa7814fec96701d01dc7e4cdd228
walter-cumbucane <cumbucane.walter@gmail.com>
2023-08-08 03:11:18 +0200

    Task 3 done

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -3,26 +3,6 @@
 #include <fcntl.h>
 #include <stdio.h>
 
-/**
- * err - set the integer to 402
- * @s: a pointer the integer we want to set to 98
- * @a: a flag
- * Return: nothing
- */
-
-void err(char *s, int a)
-{
-	if (a == 0)
-	{
-		dprintf(STDERR_FILENO, "Error : Can't read from file %s\n", s);
-		exit(98);
-	}
-	else
-	{
-		dprintf(STDERR_FILENO, "Error : Can't write to %s\n", s);
-		exit(99);
-	}
-}
 
 /**
  * main - Entry point
@@ -36,30 +16,34 @@ void err(char *s, int a)
 int main(int argc, char **argv)
 {
 	int fd_source, fd_destin, check, wrt, check1;
-	char *str;
+	char str[BUFSIZ];
 
-	str = malloc(BUFSIZ * sizeof(char));
-	if (str == NULL)
-		return (1);
 	if (argc != 3)
 	{
 		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
 		exit(97);
 	}
 	fd_source = open(*(argv + 1), O_RDONLY);
-	if (fd_source < 0)
-		err(*(argv + 1), 0);
+	if (fd_source == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", *(argv + 1));
+		exit(98);
+	}
 	fd_destin = open(*(argv + 2), O_WRONLY | O_TRUNC | O_CREAT, 0664);
 	while ((wrt = read(fd_source, str, BUFSIZ)) > 0)
 	{
-		if (fd_destin < 0 || write(fd_destin, str, wrt) != wrt)
+		if (fd_destin == -1 || write(fd_destin, str, wrt) != wrt)
 		{
 			close(fd_source);
-			err(*(argv + 2), 1);
+			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", *(argv + 2));
+			exit(99);
 		}
 	}
-	if (wrt < 0)
-		err(*(argv + 1), 0);
+	if (wrt == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", *(argv + 1));
+		exit(98);
+	}
 	check = close(fd_source);
 	check1 = close(fd_destin);
 	if (check < 0 || check1 < 0)
