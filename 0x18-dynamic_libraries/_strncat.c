7645d596f85a439d87caec1a0445bcc7c9dc70a5
walter-cumbucane <cumbucane.walter@gmail.com>
2023-09-18 19:20:21 +0200

    Task 0 done

--- 0x18-dynamic_libraries/_strncat.c
+++ 0x18-dynamic_libraries/_strncat.c
@@ -4,6 +4,7 @@
  * _strncat - set the integer to 402
  * @dest: a pointer the integer we want to set to 98
  * @src: the string to be concatenated
+ * @n : letter being tested
  * Return: nothing
  */
 
