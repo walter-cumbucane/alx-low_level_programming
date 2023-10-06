f19082fd8382d4f52ba20c7d3d089656bd556ef1
walter-cumbucane <cumbucane.walter@gmail.com>
2023-10-07 00:33:36 +0200

    Task 4 done

--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -11,15 +11,27 @@
 char *hash_table_get(const hash_table_t *ht, const char *key)
 {
 	unsigned long int i;
+	hash_node_t *node;
 
 	if (ht == NULL || key == NULL)
 		return (NULL);
+	node = malloc(sizeof(hash_node_t));
 	for (i = 0; i < ht->size; i++)
 	{
 		if (ht->array[i] != NULL && ht->array[i]->key != NULL)
 		{
 			if (strcmp(key, ht->array[i]->key) == 0)
 				return (ht->array[i]->value);
+			if (ht->array[i]->next != NULL)
+			{
+				node = ht->array[i]->next;
+				while (node != NULL)
+				{
+					if (strcmp(key, node->key) == 0)
+						return (node->value);
+					node = node->next;
+				}
+			}
 		}
 	}
 	return (NULL);
