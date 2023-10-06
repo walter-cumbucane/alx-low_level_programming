10f4eec5fceb4a102e5c8e935a8c0fdf8d102f66
walter-cumbucane <cumbucane.walter@gmail.com>
2023-10-06 21:24:30 +0200

    Task 3 in the cooking...

---
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -0,0 +1,15 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_set - adds an element to the hash table
+ * @ht : the hash table
+ * @key: the key to be stored
+ * @value: the value associated with the key
+ *
+ * Return: 1 on success and 0 on failure
+ */
+
+int hash_table_set(hash_table_t *ht, const char *key, const char *value)
+{
+	return (1);
+}
--- 0x1A-hash_tables/hash_tables.h
+++ 0x1A-hash_tables/hash_tables.h
@@ -41,4 +41,7 @@ unsigned long int hash_djb2(const unsigned char *str);
 /* Task 2 function. */
 unsigned long int key_index(const unsigned char *key, unsigned long int size);
 
+/* Task 3 function */
+int hash_table_set(hash_table_t *ht, const char *key, const char *value);
+
 #endif
