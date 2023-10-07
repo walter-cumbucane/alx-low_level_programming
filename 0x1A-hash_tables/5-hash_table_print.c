f938bc321f48f7ba89b0e86db73517b3fd53c1db
walter-cumbucane <cumbucane.walter@gmail.com>
2023-10-07 03:01:50 +0200

    Task 6 in the cooking

--- 0x1A-hash_tables/5-hash_table_print.c
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -15,8 +15,7 @@ void hash_table_print(const hash_table_t *ht)
 
 	if (ht == NULL)
 		return;
-	node = malloc(sizeof(hash_node_t));
-	printf("{");
+	putchar('{');
 	for (i = 0; i < ht->size; i++)
 	{
 		if (ht->array[i] != NULL)
